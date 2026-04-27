b4694ab3adee85a7a09d5f2c2a34d3ee32002742
Swaraj-Singh-30 <swarajsingh3020@gmail.com>
2026-04-27 14:17:09 +0530

    change ascii logo

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main(){
+func main() {
 	p := tea.NewProgram(initialModel())
 
-	if _,err := p.Run(); err!= nil{
-		println("Error: ", err.Error())
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 }
